8b32d91b98cce1ccb920a2173f571b5b266342e4
piratemermaid <schnur.ariel@gmail.com>
2017-08-15 17:16:17 -0400

    Update todo count

--- README.txt
+++ README.txt
@@ -2,13 +2,12 @@ A to do app created with vanilla Javascript.
 
 Done:
 -Add new todos
+-Update todo count
 
 To do:
-
 -Check off todos
 -Delete todos
 -Edit todos
--Update todo count
 -Mark all as completed
 -Clear completed
 -Filter All/Active/Completed
--- app.js
+++ app.js
@@ -1,23 +1,53 @@
 var DOMstrings = {
-  todoEntry: '.enter-todo'
+  todoEntry: '.enter-todo',
+  todoCount: '.todo-count'
 };
 
 var itemList = [];
 
 document.querySelector(DOMstrings.todoEntry).addEventListener('keypress', function(e) {
+  var newItem;
+
   if (e.keyCode === 13 || e.which === 13) {
-    ctrlAddItem();
+    newItem = document.querySelector(DOMstrings.todoEntry).value;
+    if (newItem !== '') {
+      ctrlAddItem(newItem);
+      updateCount();
+    }
   }
 })
 
-var ctrlAddItem = function() {
-  var newItem, index;
-  newItem = document.querySelector(DOMstrings.todoEntry).value;
-  if(newItem !== '') {
-    itemList.push(newItem);
-    index = itemList.length - 1;
-    html = '<div class="item-check"></div><div class="item-text">' + newItem + '</div><div class="item-remove">X</div><br/>';
-    document.querySelector('.main').insertAdjacentHTML('beforeend', html);
-    document.querySelector(DOMstrings.todoEntry).value = '';
+var ctrlAddItem = function(item) {
+  var index;
+  itemList.push(item);
+  index = itemList.length - 1;
+  html = '<div class="item-check"></div><div class="item-text">' + item + '</div><div class="item-remove">X</div><br/>';
+  document.querySelector('.main').insertAdjacentHTML('beforeend', html);
+  document.querySelector(DOMstrings.todoEntry).value = '';
+}
+
+var updateCount = function() {
+  var count;
+
+  count = itemList.length;
+
+  if(count === 1) {
+    show(DOMstrings.todoCount);
+    document.querySelector(DOMstrings.todoCount).textContent = '1 item left to do';
   }
+  else if(count > 1) {
+    show(DOMstrings.todoCount);
+    document.querySelector(DOMstrings.todoCount).textContent = count + ' items left to do';
+  }
+  else {
+    hide(DOMstrings.todoCount);
+  }
+}
+
+var show = function(str) {
+  document.querySelector(str).style.visiblity = "visible";
+}
+
+var hide = function(str) {
+  document.querySelector(str).style.visibility = "hidden";
 }
